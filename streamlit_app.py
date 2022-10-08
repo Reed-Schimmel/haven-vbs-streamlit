abb65bc6cfa2f987722c082a001486d4649523a6
Reed <3477188+Reed-Schimmel@users.noreply.github.com>
2022-10-08 11:46:06 -0500

    Version 0.0.2 electric boogaloo (#3)
    * hide static parameters in sidebar
    
    * protocol health config
    
    * moved logic into vbs. wip
    
    * added test csv
    
    * test data corrections
    
    * test framework works
    
    * testing error fix
    
    * add requested reset button
    
    * spread ratio always >= 0
    
    * test toggle

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,12 @@
-import math
-
 import pandas as pd
 import streamlit as st
 
-from vbs import specific_offshore, specific_onshore, max_offshore, max_onshore
+from vbs import shore
 
 
-APP_VERSION = "0.0.1"
+APP_VERSION = "0.0.2"
 PROPOSAL_VERSION = 4
-
+# TESTING = True
 
 st.set_page_config(
     page_title="VBS Simulator",
@@ -21,114 +19,106 @@ st.set_page_config(
     'About': f"[Haven Protocol](https://havenprotocol.org/). Version {APP_VERSION}"#"[GitHub](https://github.com/Reed-Schimmel/haven-vbs-streamlit)"#\n-[Haven Protocol](https://havenprotocol.org/)"
     })
 
-st.title("VBS Simulator")
-st.caption(f"Proposal Version {PROPOSAL_VERSION} | Simulator Version {APP_VERSION}")
-# st.caption(f"Simulator Version {APP_VERSION}")
-
-
 # State Initialization
 if 'simulation_list' not in st.session_state:
     st.session_state['simulation_list'] = []
+if 'static_parameters' not in st.session_state:
+    st.session_state['static_parameters'] = {}
 
-with st.expander("Static Parameters", expanded=False):
-    min_vbs = st.number_input('Minimum VBS', value=1)
-    min_shore_amount = st.number_input('Minimum amount of XHV that can be on/offshored', value=1)
-    block_cap_mult = st.number_input('Multiplier which calculates the amount one can on/offshore inside a single block', value=2500)
-    mcap_ratio_mult = st.number_input('Multiplier for working out the mcap VBS', value=40)
-    slippage_mult_good = st.number_input('During a good state, mcap ratio < 0.1, we apply a lower multiplier', value=3)
-    slippage_mult_bad = st.number_input('During a bad state, mcap ratio >= 0.1, we apply a higher multiplier', value=10)
+with st.sidebar:
+    show_tests = st.checkbox("Show tests", False)
+    with st.expander("Static Parameters", expanded=False):
+        # TODO: make this invisible to make xKleinroy happy
+        st.caption('The default values are set to the latest official proposal. Adjusting these is only for hypothetical VBS rules. These changes must be made before the first simulation.')
+        st.session_state['static_parameters'] = dict(
+            min_vbs = st.number_input('Minimum VBS', value=1),
+            min_shore_amount = st.number_input('Minimum amount of XHV that can be on/offshored', value=1),
+            block_cap_mult   = st.number_input('Multiplier which calculates the amount one can on/offshore inside a single block', value=2500),
+            mcap_ratio_mult  = st.number_input('Multiplier for working out the mcap VBS', value=40),
+            
+            # For changing the condition for "good" and "bad" protocol state
+            state_mcap_ratio = st.number_input(
+                label='Protocol state threshold',
+                help='mcap ratio < this == bad state. mcap ratio >= this == good state.',
+                value=0.9),
+            slippage_mult_good = st.number_input('During a good state, mcap ratio < "Protocol state threshold", we apply a lower multiplier', value=3),
+            slippage_mult_bad  = st.number_input('During a bad state, mcap ratio >= "Protocol state threshold", we apply a higher multiplier', value=10),
 
-    locktime_offshore = st.number_input('Offshore lock time (days)', value=21, min_value=0, step=1)
-    locktime_onshore = st.number_input('Onshore lock time (days)', value=21, min_value=0, step=1)
-    conversion_fee_offshore = st.number_input('Offshore conversion fee (%)', value=1.5, min_value=0.0, step=0.1)
-    conversion_fee_onshore = st.number_input('Onshore conversion fee (%)', value=1.5, min_value=0.0, step=0.1)
+            locktime_offshore = st.number_input('Offshore lock time (days)', value=21, min_value=0, step=1),
+            locktime_onshore  = st.number_input('Onshore lock time (days)',  value=21, min_value=0, step=1),
+            conversion_fee_offshore = st.number_input('Offshore conversion fee (%)', value=1.5, min_value=0.0, step=0.1),
+            conversion_fee_onshore  = st.number_input('Onshore conversion fee (%)',  value=1.5, min_value=0.0, step=0.1),
+        )
 
+st.title("VBS Simulator")
+st.caption(f"Proposal Version {PROPOSAL_VERSION} | Simulator Version {APP_VERSION}")
 st.markdown('---')
 
-shore_type = st.radio("Shore Type", ["Onshore", "Offshore"])
-xhv_price = st.number_input("XHV Price", min_value=0.00001, value=0.5, step=0.10)
-xhv_qty = st.number_input("Amount of unlocked XHV in vault", min_value=0.0, step=10000.0)
-xusd_qty = st.number_input("Amount of unlocked xUSD in vault", min_value=0.0, step=10000.0)
-xhv_supply = st.number_input("Number of XHV in circulation", min_value=xhv_qty, value=2.8*10E6, max_value=10E12, step=10000.0)
+# Simulation inputs
+shore_type   = st.radio("Shore Type", ["Onshore", "Offshore"])
+xhv_price    = st.number_input("XHV Price", min_value=0.00001, value=0.5, step=0.10)
+xhv_qty      = st.number_input("Amount of unlocked XHV in vault", min_value=0.0, step=10000.0)
+xusd_qty     = st.number_input("Amount of unlocked xUSD in vault", min_value=0.0, step=10000.0)
+xhv_supply   = st.number_input("Number of XHV in circulation", min_value=xhv_qty, value=2.8*10E6, max_value=10E12, step=10000.0)
 xassets_mcap = st.number_input("Market cap of all assets (in USD)", min_value=0.01, value=1.6*10E6, max_value=10E12, step=10000.0)
-
-# Calculations
-xhv_mcap = xhv_price * xhv_supply
-mcap_ratio = xassets_mcap / xhv_mcap
-spread_ratio = 1 - mcap_ratio
-
-# page 10 of PDF v4
-mcap_vbs = math.exp((mcap_ratio + math.sqrt(mcap_ratio)) * 2) - 0.5 if mcap_ratio < 0.9 else \
-           math.sqrt(mcap_ratio) * mcap_ratio_mult
-
-# TODO: make spread_vbs work
-spread_vbs = "FIX"#math.exp(1 + math.sqrt(spread_ratio)) + mcap_vbs + 1.5
-
-# TODO: offshore slippage page 12
-# slippage_vbs = math.sqrt(mcap)
-
-# Simulation Values
-sim = {
-    "Shore Type": shore_type,
-    "XHV (vault)": xhv_qty,
-    "xUSD (vault)": xusd_qty,
-    "XHV Supply": xhv_supply,
-    "XHV Price ($)": xhv_price,
-    "XHV Mcap ($)": xhv_mcap,
-    "xAssets Mcap ($)": xassets_mcap,
-    "Mcap Ratio": mcap_ratio,
-    "Spread Ratio": spread_ratio,
-    "Mcap VBS": mcap_vbs,
-    "Spread VBS": spread_vbs,
-    "Slippage VBS": "TODO",
-    "Total VBS": "TODO",
-    "Max Offshore xUSD": "TODO",
-    "Max Offshore XHV": "TODO",
-    "Max Onshore xUSD": "TODO",
-    "Max Onshore XHV": "TODO",
-    # f"Max {shore_type} xUSD": "heyyoo",
-    # f"Max {shore_type} XHV": "heyyoo",
-    "Elapsed days": (len(st.session_state['simulation_list']) + 1) * 21,
-    }
-
-# TODO: send `sim` to functions
-# TODO: update sim with returned values
-
-# sim[f"Max {shore_type} xUSD"] = "Heyyoo"
-# sim[f"Max {shore_type} XHV"] = "Heyyoo"
-
-# if shore_type == "Onshore":
-#     sim["Max Onshore xUSD"] = "TODO"
-#     sim["Max Onshore XHV"] = "TODO"
 
 # button to add simulation
 if st.button(label="Add simulation to table"):
+    sim = shore(
+        shore_type,
+        xhv_price,
+        xhv_qty,
+        xusd_qty,
+        xhv_supply,
+        xassets_mcap,
+    )
     st.session_state['simulation_list'].append(sim)
 
 st.table(st.session_state['simulation_list'])
 
-# st.write(math.exp(1))
+if st.session_state['simulation_list'] != []:
+    if st.button(label="Reset"):
+        st.session_state['simulation_list'] = []
+        st.experimental_rerun()
 
-# Tests
-# {
-#         "Shore Type": shore_type,
-#         "XHV (vault)": xhv_qty,
-#         "xUSD (vault)": xusd_qty,
-#         "XHV Supply": xhv_supply,
-#         "XHV Price ($)": xhv_price,
-#         "XHV Mcap ($)": xhv_mcap,
-#         "xAssets Mcap ($)": xassets_mcap,
-#         # "XHV Mcap": f"${xhv_price * xhv_supply}",
-#         # "xAssets Mcap": f"${xassets_mcap}",
-#         "Mcap Ratio": mcap_ratio,
-#         "Spread Ratio": "TODO",
-#         "Mcap VBS": mcap_vbs,
-#         "Spread VBS": "TODO",
-#         "Slippage VBS": "TODO",
-#         "Total VBS": "TODO",
-#         "Max Offshore xUSD": "TODO",
-#         "Max Offhore XHV": "TODO",
-#         "Max Onshore xUSD": "TODO",
-#         "Max Onshore XHV": "TODO",
-#         "Elapsed days": (len(st.session_state['simulation_list']) + 1) * 21,
-#         }
+def test_row(row):
+    return shore( # TODO: <--------------------------------- test row
+        shore_type=row['Shore Type'],
+        xhv_price=row['XHV Price ($)'],#.astype(float),
+        xhv_qty=row['XHV (vault)'],#.astype(float),
+        xusd_qty=row['xUSD (vault)'],#.astype(float),
+        xhv_supply=row['XHV Supply'],#.astype(float),
+        xassets_mcap=row['xAssets Mcap ($)'],#.astype(float),
+    )
+
+def test_df(df):
+    # return df.apply(lambda x: [*test_row(x)], axis=1)
+    # return df.transform(test_row, axis=1)
+    return [test_row(row) for index, row in df.iterrows()]
+
+if show_tests:
+    # TODO: compare tables to show where I'm wrong
+    st.markdown('---')
+    st.markdown('# Tests')
+    st.caption("The tests below are using the old data from the PDF v4 tables. v0.0.3 will have the new data recently provided by xKleinroy.")
+
+    st.markdown('## Simulation 1')
+    st.text('Reference')
+    test_df_1 = pd.read_csv('tests/test_sim_1.csv', sep=';', index_col=False)
+    st.dataframe(test_df_1)
+    st.text('Output')
+    st.table(test_df(test_df_1))
+
+    st.markdown('## Simulation 2')
+    st.text('Reference')
+    test_df_2 = pd.read_csv('tests/test_sim_2.csv', sep=';', index_col=False)
+    st.dataframe(test_df_2)
+    st.text('Output')
+    st.table(test_df(test_df_2))
+
+    st.markdown('## Simulation 3')
+    st.text('Reference')
+    test_df_3 = pd.read_csv('tests/test_sim_3.csv', sep=';', index_col=False)
+    st.dataframe(test_df_3)
+    st.text('Output')
+    st.table(test_df(test_df_3))
