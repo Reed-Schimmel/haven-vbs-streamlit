5ccdc5a9fbb862bf578f33d4f3e24de4fff58174
Reed Schimmel <reedschimmel@gmail.com>
2022-10-31 19:46:26 -0500

    calc current vbs refactor

--- vault_backed_shoring/base.py
+++ vault_backed_shoring/base.py
@@ -3,3 +3,7 @@ import math
 # v2
 def calc_block_cap(xhv_mcap, xhv_supply, block_cap_mult=3000, block_cap_exp=0.42, supply_pct=0.005):
     return ((xhv_mcap * block_cap_mult) ** block_cap_exp) + (xhv_supply * supply_pct)
+
+def calc_current_vbs(mcap_ratio, mcap_ratio_mult, thresh=0.9): # TODO: should it be < or <= thresh?
+    return (math.exp((mcap_ratio + math.sqrt(mcap_ratio)) * 2) - 0.5) if mcap_ratio < thresh else \
+           (math.sqrt(mcap_ratio) * mcap_ratio_mult)
