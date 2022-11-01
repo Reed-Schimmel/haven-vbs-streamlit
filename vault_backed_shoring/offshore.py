cef95c66bdc52ff7db4ea436a44b5c716adbf4ae
Reed Schimmel <reedschimmel@gmail.com>
2022-11-01 01:04:10 -0500

    max offshore draft

--- vault_backed_shoring/offshore.py
+++ vault_backed_shoring/offshore.py
@@ -70,7 +70,7 @@ def specific_offshore( # TODO: restore docstring
         current_vbs = calc_current_vbs(mcap_ratio, static_parameters['mcap_ratio_mult'])
     increase_ratio = abs(increase_ratio)
 
-    slippage_mult = static_parameters['slippage_mult_good'] if mcap_ratio < 0.1 else static_parameters['slippage_mult_bad']
+    slippage_mult = static_parameters['slippage_mult_good'] if new_mcap_ratio < 0.1 else static_parameters['slippage_mult_bad']
     slippage_vbs = calc_slippage_vbs(increase_ratio, slippage_mult)
 
     # set min or max VBS if the calculated VBS is out of bounds
@@ -94,7 +94,7 @@ def specific_offshore( # TODO: restore docstring
         # 'xAssets Mcap': xassets_mcap,
         # 'Mcap Ratio': mcap_ratio, # line 10 of the csv is wack yo TODO: fix??
         # 'Spread Ratio': spread_ratio,
-        # 'Mcap VBS': mcap_vbs, # TODO: problem here?
+        # 'Mcap VBS': current_vbs, # TODO: problem here?
         # 'Spread VBS': spread_vbs,
         'Slippage VBS': slippage_vbs,
         'Total VBS': total_vbs,
@@ -111,27 +111,108 @@ def max_offshore(
     xusd_vault,
     xhv_supply,
     xassets_mcap,
+
+    static_parameters,
     ):
     '''The “max” functions are intended for when the user wants to offshore or onshore the
     maximum amount possible. This will allow us to introduce a “Max” button in the vault,
     which will save users a lot of time trying to guess what that maximum value might be.'''
 
     xhv_mcap = xhv_price * xhv_supply
+    results = {
+        'Shore Type': 'Offshore',
+        'XHV (vault)': xhv_vault,
+        # 'XHV to offshore': xhv_to_offshore,
+        'xUSD (vault)': xusd_vault,
+        # 'xUSD to onshore': xusd_to_onshore,
+        'XHV Supply': xhv_supply,
+        'XHV Price': xhv_price,
+        'XHV Mcap': xhv_mcap,
+        'xAssets Mcap': xassets_mcap,
+        'Mcap Ratio': 0, # line 10 of the csv is wack yo TODO: fix??
+        'Spread Ratio': 0,
+        'Mcap VBS': 0, 
+        'Spread VBS': 0,
+        'Slippage VBS': 0,
+        'Total VBS': 0,
+        'Max Offshore XHV': 0,
+        'Max Onshore xUSD': 0,
+        'Collateral Needed (XHV)': 0,
+        'Error Message': np.nan,
+    }
+    if xhv_vault < static_parameters['min_shore_amount']:
+        results['Error Message'] = 'not enough unlocked XHV available' # -1
+        return results
 
-    init_results = specific_offshore(
-        xhv_vault,
-        xhv_vault,
-        xusd_vault,
-        0,
-        xhv_price,
-        xhv_supply,
-        xassets_mcap,
-        static_parameters)
+    block_cap = calc_block_cap(xhv_mcap, xhv_supply, static_parameters['block_cap_mult'])
+    mcap_ratio   = max(xassets_mcap / xhv_mcap, 0) # cannot be < 0
+    current_vbs = calc_current_vbs(mcap_ratio, static_parameters['mcap_ratio_mult'])
 
-    if init_results['Error Message'] != np.nan:
-        return init_results # was an error
-        # TODO: ing I was here <--------------------------
+    max_offshore_amount = xhv_vault / (current_vbs + 1)
+    new_mcap_ratio = ((max_offshore_amount * xhv_price) + xassets_mcap ) / ((xhv_supply - max_offshore_amount) * xhv_price)
+    if mcap_ratio <= 0:
+        increase_ratio = new_mcap_ratio
+    else:
+        increase_ratio = (new_mcap_ratio / mcap_ratio) - 1
+    increase_ratio = abs(increase_ratio)
 
-    
+    slippage_mult = static_parameters['slippage_mult_good'] if new_mcap_ratio < 0.1 else static_parameters['slippage_mult_bad']
+    slippage_vbs = calc_slippage_vbs(increase_ratio, slippage_mult)
+    total_vbs = max(current_vbs + slippage_vbs, static_parameters['min_vbs'])
 
-    # return results
+    temp_offshore_amount = math.floor( xhv_vault / (total_vbs + 1) )
+    offshore_acc = 1
+    temp_collateral = 0
+    collateral_diff = 0
+    additional_offshore = 0
+    prev_offshore_amount = temp_offshore_amount
+    while offshore_acc > OFFSHORE_ACC_THRESH:
+        temp_spec_result = specific_offshore(xhv_vault, temp_offshore_amount, xusd_vault, 0, xhv_price, xhv_supply, xassets_mcap, static_parameters)
+        temp_collateral = temp_spec_result['Collateral Needed (XHV)']
+        if temp_spec_result['Error Message'] == 'not enough collateral available':
+            temp_offshore_amount = prev_offshore_amount
+            break
+        if temp_collateral >= xhv_vault:
+            break
+
+        collateral_diff = (xhv_vault - temp_collateral) * temp_collateral / xhv_vault
+        offshore_acc = collateral_diff / xhv_vault
+        if offshore_acc < OFFSHORE_ACC_THRESH:
+            break
+
+        prev_offshore_amount = temp_offshore_amount
+        additional_offshore = collateral_diff / (total_vbs + 1)
+        temp_offshore_amount += additional_offshore
+
+    actual_offshore_amount = math.floor(temp_offshore_amount)
+
+    if actual_offshore_amount < static_parameters['min_shore_amount']: # TODO: separate on and offshore min amounts
+        results['Error Message'] = 'incorrect offshore amount' # -2
+        return results
+
+    if actual_offshore_amount > block_cap:
+        results['Error Message'] = 'offshore amount greater than block limit' # -3
+        return results
+
+    results.update({
+        # 'Shore Type': 'Onshore Specific',
+        # 'XHV (vault)': xhv_vault,
+        # 'XHV to offshore': xhv_to_offshore,
+        # 'xUSD (vault)': xusd_vault,
+        # 'xUSD to onshore': xusd_to_onshore,
+        # 'XHV Supply': xhv_supply,
+        # 'XHV Price': xhv_price,
+        # 'XHV Mcap': xhv_mcap,
+        # 'xAssets Mcap': xassets_mcap,
+        'Mcap Ratio': mcap_ratio, # line 10 of the csv is wack yo TODO: fix??
+        'Spread Ratio': spread_ratio,
+        'Mcap VBS': mcap_vbs, # TODO: problem here?
+        'Spread VBS': spread_vbs,
+        'Slippage VBS': slippage_vbs,
+        'Total VBS': total_vbs, # TODO or should it be? temp_spec_result['Total VBS'],
+        'Max Offshore XHV': actual_offshore_amount,
+        # 'Max Onshore xUSD': -1,
+        'Collateral Needed (XHV)': total_collateral,
+        # 'Error Message': err_msg,
+    })
+    return results
